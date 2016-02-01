c917e59148817ab95b96d7e62d2e3007596f8f92
Zib Redlektab <zib@zibland.com>
2016-02-01 15:42:21 -0500

    some minor progress - nodes can be entered, and their contents are loaded into qcgui.

--- QuestCore/Source/QuestCore/QuestCore.Build.cs
+++ QuestCore/Source/QuestCore/QuestCore.Build.cs
@@ -6,12 +6,14 @@ public class QuestCore : ModuleRules
 {
 	public QuestCore(TargetInfo Target)
 	{
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", 
+            "UMG", "Slate", "SlateCore"
+        });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
