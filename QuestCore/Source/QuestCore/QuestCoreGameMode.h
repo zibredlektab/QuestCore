ef484d69294af3b3041cfcc1d0958fcf01bb0897
Zib Redlektab <zib@zibland.com>
2016-02-15 20:11:30 -0500

    More of the same

---
+++ QuestCore/Source/QuestCore/QuestCoreGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "QuestCoreGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class QUESTCORE_API AQuestCoreGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
